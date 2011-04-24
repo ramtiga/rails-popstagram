cbbb9436d3c9e53dc0f4bf6bfd189f2142115f05
t_ramu <dhanegm731@gmail.com>
2011-04-24 18:05:01 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Popstagram::Application.config.secret_token = '66f69b9223992941b6d71ed8f7b84eb16070293bc52b84b1b8bb364faa0464cd75fe69aac616b621e3292db0940f9759d7f978a6523ac62fcd93cfd818f1794f'
